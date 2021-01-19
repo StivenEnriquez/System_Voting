fe5637f73425b9853b9cb6d3ef8d0724f8d3807b
Stiven Eneiquez <cristianangulo1@hotmail.com>
2021-01-19 12:27:53 -0500

    Primer sprint y login

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+@section('content')
+ <link rel="stylesheet" href="{{asset('css/estilo.css')}}" crossorigin="anonymous">
+ <header class="main-header">
+      <div class="background-overlay text-white py-5" >
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 text-center justify-content-center align-self-center"> 
+               <h1>
+                 Bienvenidos Al Sistema De votacion Estudiantil
+               </h1>
+                 
+            </div>
+          </div>
+        </div>
+      </div>
+    </header> 
+@stop
